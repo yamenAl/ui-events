ec3e88816c3601c4eabe09890f180facbae5e129
Krijn Hoetmer <krijn@qontent.nl>
2024-12-08 20:11:01 +0100

    Add comments to code

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,23 @@
+// De eerste interactie is al voor jullie uitgewerkt
+// Als je op het 12e linkje klikt (“Interaction”), springt deze omhoog
+
+// Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
 let interaction = document.querySelector('a:nth-of-type(12)')
 
+// Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
+// In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
 interaction.addEventListener('click', jumpHandler)
-interaction.addEventListener('animationend', jumpHandler)
 
+// Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
+// _niet_ meteen bij het laden van de pagina aangeroepen
 function jumpHandler() {
+  
+  // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
   interaction.classList.toggle('jump')
 }
+
+// Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
+interaction.addEventListener('animationend', jumpHandler)
+
+
+// Ga zelf verder met de overige elementen, aan de hand van de instructies
